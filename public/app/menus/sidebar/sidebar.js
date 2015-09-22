4d2c04d0b616226b467ea37ce3f3011b52abc8f0
Hermann POILPRE <hermannpoilpre@gmail.com>
2015-09-22 18:15:53 +0200

    no message

--- public/app/menus/sidebar/sidebar.js
+++ public/app/menus/sidebar/sidebar.js
@@ -1,5 +1,30 @@
 angular.module('sidebar', [])
     .controller('SidebarCtrl', function SidebarCtrl(){
         var sidebarCtrl = this;
+        sidebarCtrl.menu = [
+          {
+            text:"Dashboard",
+            icon:"menu/ic_dashboard",
+            href:""
+          },
+          {
+            text:"Patients",
+            icon:"menu/ic_patient",
+            href:"",
+            children:[
+              {
+                text:"Liste des patients",
+                icon:"",
+                href:"/patients"
+              },
+              {
+                text:"Nouveau patient",
+                icon:"",
+                href:"/patients/create"
+              }
+            ]
+          },
+
+        ]
     })
 ;
