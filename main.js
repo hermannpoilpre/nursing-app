73ebfcc69329affb9fbbd4c28025f2a3e2ea0458
Hermann POILPRE <hermannpoilpre@gmail.com>
2016-04-25 17:46:34 +0200

    no message

--- main.js
+++ main.js
@@ -4,19 +4,22 @@ var morgan = require('morgan');
 var app = express();
 var config = require('./config');
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
 app.set('superSecret', config.secret);
 
 app.use(morgan('dev'));
 
+app.get("/", function(req, res) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Welcome!');
+});
+
 app.get("/auth", function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    
+    res.setHeader(200, 'Content-Type', 'application/json');
     var result = {
         firstname: 'Elliot',
         lastname: 'Smith'
     };
-
     res.send(JSON.stringify(result));
 });
 
